64a618924c5a662130516147ad79ac9ecc6a6382
Jozeph Bautista <jozephbautista@yahoo.com>
2024-01-12 17:04:10 -0800

    Add features for data binding, bind property, and demo product component.

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -12,4 +12,6 @@ public class Demo_Product
     public string Name { get; set; }
     public  double Price { get; set; }
     public bool IsActive { get; set; }
+
+    public List<Demo_ProductProp> ProductProperties { get; set; }
 }
