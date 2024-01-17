2d853d76731b974f809cdd14aa16c54d2751a6de
Jozeph Bautista <jozephbautista@yahoo.com>
2024-01-16 16:17:35 -0800

    Add model and repository

---
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_DataAccess.Data
+{
+    //JB: Package Manager Console commands
+    // add-migration AddCategoryToDatabase
+    // update-database
+
+
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductPrice> ProductPrices { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
