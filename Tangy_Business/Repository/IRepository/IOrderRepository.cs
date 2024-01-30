bfe770463c9ce86cfed5191cab11c09ed70efc45
Jozeph Bautista <jozephbautista@yahoo.com>
2024-01-29 16:52:36 -0800

    Add Order Management component and details UI.

--- Tangy_Business/Repository/IRepository/IOrderRepository.cs
+++ Tangy_Business/Repository/IRepository/IOrderRepository.cs
@@ -6,20 +6,19 @@ using System.Threading.Tasks;
 using Tangy_DataAccess;
 using Tangy_Models;
 
-namespace Tangy_Business.Repository.IRepository
+namespace Tangy_Business.Repository.IRepository;
+
+public interface IOrderRepository
 {
-    public interface IOrderRepository
-    {
-        public Task<OrderDTO> Get(int id);
-        public Task<IEnumerable<OrderDTO>> GetAll(string? userId = null, string? status = null);
-        public Task<OrderDTO> Create(OrderDTO objDTO);
-        public Task<int> Delete(int id);
+    public Task<OrderDTO> Get(int id);
+    public Task<IEnumerable<OrderDTO>> GetAll(string? userId = null, string? status = null);
+    public Task<OrderDTO> Create(OrderDTO objDTO);
+    public Task<int> Delete(int id);
 
-        public Task<OrderHeaderDTO> UpdateHeader(OrderHeaderDTO objDTO);
+    public Task<OrderHeaderDTO> UpdateHeader(OrderHeaderDTO objDTO);
 
-        public Task<OrderHeaderDTO> MarkPaymentSuccessful(int id);
-        public Task<bool> UpdateOrderStatus(int orderId, string status);
+    public Task<OrderHeaderDTO> MarkPaymentSuccessful(int id);
+    public Task<bool> UpdateOrderStatus(int orderId, string status);
 
-        public Task<OrderHeaderDTO> CancelOrder(int id);
-    }
+    public Task<OrderHeaderDTO> CancelOrder(int id);
 }
